561c65e3a3cb59514d43720617e85bfdb5d39cf3
hluszik <43569178+hluszik@users.noreply.github.com>
2018-11-06 12:14:19 +0100

    Create aufgabe_2.2.c

---
+++ aufgabe_2.2.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+   
+  double myReadNumber (){
+  double a = 0;
+  scanf ("%lf, &a");
+  return a;
+  
+  }
+  
+  double mySquare(double a){
+	  return a*a;
+  }
+  
+  
+  double myPrintNumber(double a){
+	  printf("%lf, a);
+  }
+
+
+  
